e68ace0d5ba3a5722c83e57a0d332757d2f48826
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-15 04:55:13 +0000

    learning about nesting a list in a dictionary and visa versa.

--- main.py
+++ main.py
@@ -5,8 +5,36 @@ programming_dictionary = {
   "Loop": "The action of doing something over and over again.",
 }
 
-#calling a value from the dictionary using its key
-print(programming_dictionary["Bug"])
 
 #Adding new items to dictionary
 programming_dictionary["Something New"] = "The value of the new item to add to the dictionary"
+
+
+#looping through dictionaries
+
+#for key in programming_dictionary:
+  #print(key)
+  #print(programming_dictionary[key])
+
+
+#Nesting dictionaries and lists within a dictionary.
+travel_log = {
+  "France":{"Visited": ["Paris", "Lille", "Dijon"]},
+  "South Africa": ["Pretoria","Johannesburg", "Cape Town"],
+  "United States" : "Washington DC",
+}
+
+print(travel_log["France"])
+
+#Nesting a dictionary within a list
+travel_log =[ 
+  {
+  "country": "France",
+  "Visited": ["Paris", "Lille", "Dijon"],
+  "total_visits": 12
+  },
+  {
+    "country": "South Africa",
+    "Visited": ["Pretoria","Johannesburg","Cape Town"]
+  }
+]
