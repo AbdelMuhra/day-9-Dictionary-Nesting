c6bbd806d830014966e3677cecf63a8a937cb851
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-15 04:16:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#creating a dictionary
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again.",
+  "Loop": "The action of doing something over and over again.",
+}
+
+#calling a value from the dictionary using its key
+print(programming_dictionary["Bug"])
+
+#Adding new items to dictionary
+programming_dictionary["Something New"] = "The value of the new item to add to the dictionary"
