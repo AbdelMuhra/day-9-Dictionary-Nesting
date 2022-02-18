ff0518738c0e2496f64b9189fc4c46db316438b2
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-18 05:49:34 +0000

    fixed a bug. and made the code more readable

--- main.py
+++ main.py
@@ -2,39 +2,31 @@ from replit import clear
 from art import logo
 
 #creating a dictionary to store the bid amount and name
-bid = {
-  "Name": "abdel",
-  "Bid amount": 0,
-}
+bids = {}
 
-def auction():
-  highest_bidder = 0
-  highest_bidder_name = ""
-  #get username
-  live_auction = True
-  
-  while live_auction:
-    #display logo
-    print(logo)
-    name = input("What is your name?")
-    #get user's bid
-    bid_amount = int(input("What is your bid?"))
-    question = input("Any other Bidder? (Y|N)?")
-    if question == "N":
-      live_auction = False
-      print(f"The highest bidder is {highest_bidder_name} with {highest_bidder}")
-    
-    #to clear console after user has inputed their details
-    clear()
-  for buyer in bid:
-    bid["Name"] = name
-    bid["Bid amount"] = bid_amount
-    if highest_bidder > bid["Bid amount"]:
-      highest_bidder = bid["Bid amount"]
-      highest_bidder_name = bid["Name"]
+live_auction = True
 
-    
-    
-auction()
-print(bid)
+def highest_bidder(bids_record):
+  highest_bid = 0
+  winner = ""
+  for bidder in bids_record:
+    bid_amount = bids_record[bidder]
+    if bid_amount > highest_bid:
+      bid_amount = highest_bid
+      winner = bidder
+  print(f"The highest bidder is {winner} with {highest_bid}")
+
+while live_auction:
+  #display logo
+  print(logo)
+  name = input("What is your name?: ")
+  bid_amount = int(input("What is your bid: $"))
+  bids[name] = bid_amount
+  question = input("Any other Bidder? (Y|N)?")
+  clear()
+  if question == "N":
+    highest_bidder(bids)
+    live_auction = False
+
+
 
