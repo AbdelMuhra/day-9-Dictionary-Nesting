7b04ddde69a86f66bc53c67f641da22bd6bc2c9c
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-18 04:58:54 +0000

    made an auction program that allows users to enter their name and bid. The highest bidder's name and amount will be displayed at the end. (Used a dictionary to store the information)

--- main.py
+++ main.py
@@ -1,52 +1,40 @@
-#creating a dictionary
-programming_dictionary = {
-  "Bug": "An error in a program that prevents the program from running as expected.", 
-  "Function": "A piece of code that you can easily call over and over again.",
-  "Loop": "The action of doing something over and over again.",
+from replit import clear
+from art import logo
+
+#creating a dictionary to store the bid amount and name
+bid = {
+  "Name": "abdel",
+  "Bid amount": 0,
 }
 
+def auction():
+  highest_bidder = 0
+  highest_bidder_name = ""
+  #get username
+  live_auction = True
+  
+  while live_auction:
+    #display logo
+    print(logo)
+    name = input("What is your name?")
+    #get user's bid
+    bid_amount = int(input("What is your bid?"))
+    question = input("Any other Bidder? (Y|N)?")
+    if question == "N":
+      live_auction = False
+      print(f"The highest bidder is {highest_bidder_name} with {highest_bidder}")
+    
+    #to clear console after user has inputed their details
+    clear()
+  for buyer in bid:
+    bid["Name"] = name
+    bid["Bid amount"] = bid_amount
+    if highest_bidder > bid["Bid amount"]:
+      highest_bidder = bid["Bid amount"]
+      highest_bidder_name = bid["Name"]
 
-#Adding new items to dictionary
-programming_dictionary["Something New"] = "The value of the new item to add to the dictionary"
+    
+    
+auction()
+print(bid)
 
-
-#looping through dictionaries
-
-#for key in programming_dictionary:
-  #print(key)
-  #print(programming_dictionary[key])
-
-
-#Nesting dictionaries and lists within a dictionary.
-travel_log = {
-  "France":{"Visited": ["Paris", "Lille", "Dijon"]},
-  "South Africa": ["Pretoria","Johannesburg", "Cape Town"],
-  "United States" : "Washington DC",
-}
-
-print(travel_log["France"])
-
-#Nesting a dictionary within a list
-travel_log =[ 
-  {
-  "country": "France",
-  "Visited": ["Paris", "Lille", "Dijon"],
-  "total_visits": 12
-  },
-  {
-    "country": "South Africa",
-    "Visited": ["Pretoria","Johannesburg","Cape Town"]
-  }
-]
-
-#function to add a new country to the dictionary
-def add_new_country(country_name, visits_num, visited_cities):
-    add_country = {}
-    add_country["country"] = country_name
-    add_country["visits"] = visits_num
-    add_country["cities"] = visited_cities
-    travel_log.append(add_country)
-
-
-add_new_country("Syria", 2, ["Damascus", "Allepo"])
-print(travel_log)
