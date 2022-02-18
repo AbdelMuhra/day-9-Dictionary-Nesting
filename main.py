3388f5a79a9c704b6b4a30c2df4bef8026e35b9a
Abdel Ghani Muhra <67690813+AbdelMuhra@users.noreply.github.com>
2022-02-18 05:53:01 +0000

    fixed another big. code works perfectly now.

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 from replit import clear
 from art import logo
 
-#creating a dictionary to store the bid amount and name
+print(logo)
+
 bids = {}
+bidding_finished = False
 
-live_auction = True
-
-def highest_bidder(bids_record):
+def find_highest_bidder(bidding_record):
   highest_bid = 0
   winner = ""
-  for bidder in bids_record:
-    bid_amount = bids_record[bidder]
-    if bid_amount > highest_bid:
-      bid_amount = highest_bid
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
       winner = bidder
-  print(f"The highest bidder is {winner} with {highest_bid}")
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
 
-while live_auction:
-  #display logo
-  print(logo)
+while not bidding_finished:
   name = input("What is your name?: ")
-  bid_amount = int(input("What is your bid: $"))
-  bids[name] = bid_amount
-  question = input("Any other Bidder? (Y|N)?")
-  clear()
-  if question == "N":
-    highest_bidder(bids)
-    live_auction = False
-
-
-
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if should_continue == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    clear()
